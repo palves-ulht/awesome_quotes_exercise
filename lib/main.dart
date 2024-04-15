9f487aa25ec1dd7f41bb4dbcb23f266fc180c920
Pedro Alves <pedro.alves@ulusofona.pt>
2024-04-15 10:13:24 +0100

    Exercício passa a usar o Provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:awesome_quotes_exercise/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  // TODO passar a usar o MultiProvider para injetar um FavoritesModel e um QuotesService
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // TODO passar a usar o MultiProvider para injetar um FavoritesModel e um QuotesService
   const MyApp({super.key});
 
   // This widget is the root of your application.
